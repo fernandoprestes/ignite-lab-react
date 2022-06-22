eccc79cdafe32b70e68b82a9e591c8392983e065
fernandoprestes <fernandoprestesc@gmail.com>
2022-06-22 20:31:06 -0300

    feat: add sidebar component

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,51 @@
+import { CheckCircle, Lock } from 'phosphor-react';
+import { isPast, format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class';
+}
+
+export const Lesson = function LessonComponent({
+  title,
+  slug,
+  availableAt,
+  type,
+}: LessonProps) {
+  const isLessonAvailable = isPast(availableAt);
+  const availableDateFormatted = format(
+    availableAt,
+    "EEEE' • 'd' de 'MMMM' • 'k'h'mm",
+    {
+      locale: ptBR,
+    },
+  );
+
+  return (
+    <a href='#'>
+      <span className='text-gray-300'>{availableDateFormatted}</span>
+      <div className='mt-2 rounded border border-gray-500 p-4'>
+        <header className='flex items-center justify-between'>
+          {isLessonAvailable ? (
+            <span className='flex items-center gap-2 text-sm font-medium text-blue-500'>
+              <CheckCircle size={20} />
+              Conteúdo liberado
+            </span>
+          ) : (
+            <span className='flex items-center gap-2 text-sm font-medium text-orange-500'>
+              <Lock size={20} />
+              Em Breve
+            </span>
+          )}
+          <span className='rounded border border-green-300 px-2 py-[0.125rem] text-xs font-bold'>
+            {type === 'live' ? 'Ao Vivo' : 'Aula prática'}
+          </span>
+        </header>
+        <strong className='mt-5 block text-gray-200'>{title}</strong>
+      </div>
+    </a>
+  );
+};
---
+++ src/components/Video.tsx
@@ -0,0 +1,3 @@
+export const Video = function VideoComponent() {
+  return <div className='flex-1'>Video</div>;
+};
